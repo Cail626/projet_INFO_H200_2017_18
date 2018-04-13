bfcdfccc5bf2d6384cd0e7e228c39768fe9bd391
Cail626 <33787753+Cail626@users.noreply.github.com>
2018-04-13 09:32:14 +0200

    Update Keyboard.java

--- Controller/Keyboard.java
+++ Controller/Keyboard.java
@@ -4,10 +4,13 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import Model.Game;
+import Model.Directable;
 
 public class Keyboard implements KeyListener {
     private Game game;
-    private static final int player1 = 0;
+    private boolean inventoryState;
+    
+    private static final int player = 0;
 
     public Keyboard(Game game) {
         this.game = game;
@@ -16,25 +19,47 @@ public class Keyboard implements KeyListener {
     @Override
     public void keyPressed(KeyEvent event) {
         int key = event.getKeyCode();
-
-        switch (key) {
-        case KeyEvent.VK_RIGHT:
-            game.movePlayer(1, 0, player1);
-            break;
-        case KeyEvent.VK_LEFT:
-            game.movePlayer(-1, 0, player1);
-            break;
-        case KeyEvent.VK_DOWN:
-            game.movePlayer(0, 1, player1);
-            break;
-        case KeyEvent.VK_UP:
-            game.movePlayer(0, -1, player1);
-             break;
-         case KeyEvent.VK_SPACE:
-             game.action(player1);
-             break;
-        case KeyEvent.VK_P:
-             game.playerPos(player1);
+        
+        if(inventoryState == false){
+	        switch (key) {
+	        case KeyEvent.VK_RIGHT:
+	            game.moveCaracter(1, 0, player);
+	            break;
+	        case KeyEvent.VK_LEFT:
+	            game.moveCaracter(-1, 0, player);
+	            break;
+	        case KeyEvent.VK_DOWN:
+	            game.moveCaracter(0, 1, player);
+	            break;
+	        case KeyEvent.VK_UP:
+	            game.moveCaracter(0, -1, player);
+	             break;
+	         case KeyEvent.VK_SPACE:
+	             game.action(player);
+	             break;
+	        case KeyEvent.VK_P:
+	             game.playerPos(player);
+	             break;
+	        }
+        }
+        else{
+        	switch (key) {
+	        case KeyEvent.VK_RIGHT:
+	            game.moveIc(Directable.EAST);
+	            break;
+	        case KeyEvent.VK_LEFT:
+	        	game.moveIc(Directable.WEST);
+	            break;
+	        case KeyEvent.VK_DOWN:
+	        	game.moveIc(Directable.SOUTH);
+	            break;
+	        case KeyEvent.VK_UP:
+	        	game.moveIc(Directable.NORTH);
+	             break;
+	         case KeyEvent.VK_SPACE:
+	             game.action(player);
+	             break;
+	        }
         }
     }
 
@@ -43,6 +68,12 @@ public class Keyboard implements KeyListener {
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased(KeyEvent event) {
+    	int key = event.getKeyCode();
+    	switch (key) {
+	    case KeyEvent.VK_I:
+	    	inventoryState = game.switchInventory();
+	    	break;
+    	}
     }
 }
